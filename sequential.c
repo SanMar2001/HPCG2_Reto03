3a92c56b2bb6ae09c220984bec973220db05eb49
SanMar2001 <s.martinez4@utp.edu.co>
2025-11-28 23:52:14 -0500

    Executable generated

--- sequential.c
+++ sequential.c
@@ -2,84 +2,58 @@
 #include <stdlib.h>
 #include <time.h>
 
+#define ITER 1000
+
 int main(int argc, char *argv[]) {
 
     if (argc != 2) {
-        printf("Uso: %s <tamano_carretera>\n", argv[0]);
         return 1;
     }
 
     int N = atoi(argv[1]);
-    int ITER = 10000;  // Número de iteraciones (puedes modificarlo)
 
-    int *old = malloc(N * sizeof(int));
-    int *new = malloc(N * sizeof(int));
+    int size = N + 2;
 
-    if (!old || !new) {
-        printf("Error al asignar memoria\n");
-        return 1;
+    int *old = malloc(size * sizeof(int));
+    int *new = malloc(size * sizeof(int));
+
+    for (int i = 1; i <= N; i++) {
+        old[i] = rand() % 2;
     }
 
-    // Inicialización aleatoria de la carretera
-    srand(time(NULL));
-    for (int i = 0; i < N; i++) {
-        old[i] = rand() % 2;   // 0 o 1
-    }
-
-    // Contabilidad
-    int total_carros = 0;
-    for (int i = 0; i < N; i++)
-        if (old[i] == 1)
-            total_carros++;
-
-    if (total_carros == 0) {
-        printf("No hay carros en la carretera, velocidad = 0\n");
-        return 0;
-    }
-
-    long movimientos_totales = 0;
-
-    // Medición de tiempo
     clock_t start = clock();
 
-    // --- Bucle principal ---
-    for (int t = 0; t < ITER; t++) {
+    for (int iter = 0; iter < ITER; iter++) {
 
-        int movimientos_iter = 0;
+        old[0]   = old[N];
+        old[N+1] = old[1];
 
-        // Actualizamos todas las celdas
-        for (int i = 0; i < N; i++) {
+        for (int i = 1; i <= N; i++) {
 
-            int derecha = (i + 1) % N;
-
-            // Regla de movimiento
-            if (old[i] == 1 && old[derecha] == 0) {
-                new[i] = 0;
-                new[derecha] = 1;
-                movimientos_iter++;
-            } else if (!(old[derecha] == 1 && new[derecha] == 1)) {
-                // Solo preservamos cuando no fue sobreescrito por un movimiento
-                new[i] = old[i];
+            if (old[i] == 1) {
+                if (old[i+1] == 1)
+                    new[i] = 1;
+                else
+                    new[i] = 0;
+            }
+            else {
+                if (old[i-1] == 1)
+                    new[i] = 1;
+                else
+                    new[i] = 0;
             }
         }
 
-        movimientos_totales += movimientos_iter;
-
-        // Intercambiamos old y new
-        int *tmp = old;
-        old = new;
-        new = tmp;
+        for (int i = 1; i <= N; i++) {
+            old[i] = new[i];
+        }
     }
 
     clock_t end = clock();
 
     double tiempo = (double)(end - start) / CLOCKS_PER_SEC;
 
-    // Velocidad promedio (0-1)
-    double velocidad_promedio = (double) movimientos_totales / (double)(total_carros * ITER);
-
-    printf("Tiempo de ejecucion: %f segundos\n", tiempo);
-    printf("Velocidad promedio: %f\n", velocidad_promedio);
+    printf("%.6f\n", tiempo);
 
     free(old);
     free(new);
