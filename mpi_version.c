c33fba03482435d0cc39d4978dc71aa42f269721
SanMar2001 <s.martinez4@utp.edu.co>
2025-12-01 19:45:23 -0500

    Updated MPI version

--- mpi_version.c
+++ mpi_version.c
@@ -5,6 +5,95 @@
 
 #define ITER 1000
 
+void init_random(int *old, int local_N) {
+    for (int i = 1; i <= local_N; i++) {
+        old[i] = rand() % 2;
+    }
+}
+
+void exchange_halos_async(int *old, int local_N, int rank, int size,
+                          MPI_Request reqs[4]) {
+
+    int left  = (rank == 0)        ? size - 1 : rank - 1;
+    int right = (rank == size - 1) ? 0        : rank + 1;
+
+    MPI_Irecv(&old[0], 1, MPI_INT, left,  1, MPI_COMM_WORLD, &reqs[0]);
+    MPI_Irecv(&old[local_N+1], 1, MPI_INT, right, 0, MPI_COMM_WORLD, &reqs[1]);
+    MPI_Isend(&old[1], 1, MPI_INT, left,  0, MPI_COMM_WORLD, &reqs[2]);
+    MPI_Isend(&old[local_N], 1, MPI_INT, right, 1, MPI_COMM_WORLD, &reqs[3]);
+}
+
+void update_cells_internal(const int *old, int *new, int local_N) {
+    for (int i = 2; i <= local_N - 1; i++) {
+        if (old[i] == 1)
+            new[i] = (old[i+1] == 1) ? 1 : 0;
+        else
+            new[i] = (old[i-1] == 1) ? 1 : 0;
+    }
+}
+
+void update_cells_boundary(const int *old, int *new) {
+    if (old[1] == 1)
+        new[1] = (old[2] == 1) ? 1 : 0;
+    else
+        new[1] = (old[0] == 1) ? 1 : 0;
+
+    int N = 0;
+    int local_N = (int)((void*)(&new[0]) != NULL);
+}
+
+void update_cells_boundary_fixed(const int *old, int *new, int local_N) {
+
+    if (old[1] == 1)
+        new[1] = (old[2] == 1) ? 1 : 0;
+    else
+        new[1] = (old[0] == 1) ? 1 : 0;
+
+    if (old[local_N] == 1)
+        new[local_N] = (old[local_N+1] == 1) ? 1 : 0;
+    else
+        new[local_N] = (old[local_N-1] == 1) ? 1 : 0;
+}
+
+void copy_array(const int *src, int *dst, int local_N) {
+    for (int i = 1; i <= local_N; i++) {
+        dst[i] = src[i];
+    }
+}
+
+double run_simulation(int N, int rank, int size) {
+
+    int local_N = N / size;
+
+    int *old = malloc((local_N + 2) * sizeof(int));
+    int *new = malloc((local_N + 2) * sizeof(int));
+
+    srand(time(NULL) + rank * 1234);
+    init_random(old, local_N);
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    double start = MPI_Wtime();
+
+    MPI_Request reqs[4];
+
+    for (int iter = 0; iter < ITER; iter++) {
+        exchange_halos_async(old, local_N, rank, size, reqs);
+        if (local_N > 2)
+            update_cells_internal(old, new, local_N);
+        MPI_Waitall(4, reqs, MPI_STATUSES_IGNORE);
+        update_cells_boundary_fixed(old, new, local_N);
+        copy_array(new, old, local_N);
+    }
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    double end = MPI_Wtime();
+
+    free(old);
+    free(new);
+
+    return end - start;
+}
+
 int main(int argc, char *argv[]) {
 
     MPI_Init(&argc, &argv);
@@ -14,69 +103,16 @@ int main(int argc, char *argv[]) {
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
     if (argc != 2) {
-        if (rank == 0) printf("Uso: mpirun -np <p> ./prog N\n");
         MPI_Finalize();
         return 1;
     }
 
     int N = atoi(argv[1]);
 
-    // División uniforme
-    int local_N = N / size;
-    int start = rank * local_N + 1;
-
-    // Reservar con ghosts
-    int *old = malloc((local_N + 2) * sizeof(int));
-    int *new = malloc((local_N + 2) * sizeof(int));
-
-    // Inicialización consistente con secuencial
-    srand(time(NULL) + rank);
-    for (int i = 1; i <= local_N; i++)
-        old[i] = rand() % 2;
-
-    MPI_Barrier(MPI_COMM_WORLD);
-    double start_t = MPI_Wtime();
-
-    for (int iter = 0; iter < ITER; iter++) {
-
-        int left  = (rank == 0) ? size - 1 : rank - 1;
-        int right = (rank == size - 1) ? 0 : rank + 1;
-
-        MPI_Request reqs[4];
-
-        // Recibir primero (no bloqueante)
-        MPI_Irecv(&old[0],      1, MPI_INT, left,  0, MPI_COMM_WORLD, &reqs[0]);
-        MPI_Irecv(&old[local_N+1], 1, MPI_INT, right, 1, MPI_COMM_WORLD, &reqs[1]);
-
-        // Enviar después (no bloqueante)
-        MPI_Isend(&old[1],         1, MPI_INT, left,  1, MPI_COMM_WORLD, &reqs[2]);
-        MPI_Isend(&old[local_N],   1, MPI_INT, right, 0, MPI_COMM_WORLD, &reqs[3]);
-
-        // Esperar todas las comm
-        MPI_Waitall(4, reqs, MPI_STATUSES_IGNORE);
-
-        // Aplicar reglas (idéntico a tu código)
-        for (int i = 1; i <= local_N; i++) {
-
-            if (old[i] == 1) {
-                new[i] = (old[i+1] == 1) ? 1 : 0;
-            } else {
-                new[i] = (old[i-1] == 1) ? 1 : 0;
-            }
-        }
-
-        // Copiar
-        for (int i = 1; i <= local_N; i++)
-            old[i] = new[i];
-    }
-
-    double end_t = MPI_Wtime();
+    double tiempo = run_simulation(N, rank, size);
 
     if (rank == 0)
-        printf("Tiempo: %.6f\n", end_t - start_t);
-
-    free(old);
-    free(new);
+        printf("%.6f\n", tiempo);
 
     MPI_Finalize();
     return 0;
--- sequential.c
+++ sequential.c
@@ -1,61 +1,72 @@
+//Version 1 - Sequential version
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
 #define ITER 1000
 
-int main(int argc, char *argv[]) {
+void init_random(int *old, int N) {
+    for (int i = 1; i <= N; i++) {
+        old[i] = rand() % 2;
+    }
+}
 
+void apply_periodic_boundaries(int *old, int N) {
+    old[0]   = old[N];
+    old[N+1] = old[1];
+}
+
+void update_cells(const int *old, int *new, int N) {
+    for (int i = 1; i <= N; i++) {
+        if (old[i] == 1) {
+            new[i] = (old[i+1] == 1) ? 1 : 0;
+        } else {
+            new[i] = (old[i-1] == 1) ? 1 : 0;
+        }
+    }
+}
+
+void copy_array(const int *src, int *dst, int N) {
+    for (int i = 1; i <= N; i++) {
+        dst[i] = src[i];
+    }
+}
+
+double run_simulation(int N) {
+    int size = N + 2;
+
+    int *old = malloc(size * sizeof(int));
+    int *new = malloc(size * sizeof(int));
+
+    init_random(old, N);
+
+    clock_t start = clock();
+
+    for (int iter = 0; iter < ITER; iter++) {
+        apply_periodic_boundaries(old, N);
+        update_cells(old, new, N);
+        copy_array(new, old, N);
+    }
+
+    clock_t end = clock();
+
+    free(old);
+    free(new);
+
+    return (double)(end - start) / CLOCKS_PER_SEC;
+}
+
+int main(int argc, char *argv[]) {
     if (argc != 2) {
         return 1;
     }
 
     int N = atoi(argv[1]);
 
-    int size = N + 2;
-
-    int *old = malloc(size * sizeof(int));
-    int *new = malloc(size * sizeof(int));
-
-    for (int i = 1; i <= N; i++) {
-        old[i] = rand() % 2;
-    }
-
-    clock_t start = clock();
-
-    for (int iter = 0; iter < ITER; iter++) {
-
-        old[0]   = old[N];
-        old[N+1] = old[1];
-
-        for (int i = 1; i <= N; i++) {
-
-            if (old[i] == 1) {
-                if (old[i+1] == 1)
-                    new[i] = 1;
-                else
-                    new[i] = 0;
-            }
-            else {
-                if (old[i-1] == 1)
-                    new[i] = 1;
-                else
-                    new[i] = 0;
-            }
-        }
-
-        for (int i = 1; i <= N; i++) {
-            old[i] = new[i];
-        }
-    }
-
-    clock_t end = clock();
-
-    double tiempo = (double)(end - start) / CLOCKS_PER_SEC;
+    double tiempo = run_simulation(N);
 
     printf("%.6f\n", tiempo);
 
-    free(old);
-    free(new);
     return 0;
 }
